53990e4ccbf7f237fe874bd5401e3beec9a8e048
rock <22974390@qq.com>
2018-11-09 10:47:18 +0800

    add sql init file2

---
+++ file/initsql/riskjingdonginit.sql
@@ -0,0 +1,134 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.244.206
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : 192.168.244.206:3306
+ Source Schema         : lu_tale
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 09/11/2018 10:43:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for risk_accredit
+-- ----------------------------
+DROP TABLE IF EXISTS `risk_accredit`;
+CREATE TABLE `risk_accredit`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `orderId` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '订单编号',
+  `userId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户ID',
+  `accreditInfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户授权信息',
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for risk_applyAmount
+-- ----------------------------
+DROP TABLE IF EXISTS `risk_applyAmount`;
+CREATE TABLE `risk_applyAmount`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `bizNo` int(11) NULL DEFAULT NULL COMMENT '业务流水号，此字段由商户生成，需确保唯一性，用于定位每一次请求',
+  `userId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商户系统内用户唯一编号',
+  `modelNo` int(11) NULL DEFAULT NULL COMMENT '规格/型号（租赁场景）',
+  `applyAmount` int(10) NULL DEFAULT NULL COMMENT '申请金额（单位元）',
+  `applyDays` varchar(4) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '申请期限(日)',
+  `applyMonths` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '申请期限（月）',
+  `contractAmount` int(10) NULL DEFAULT NULL COMMENT '租赁物品签约价（租赁场景）',
+  `createTime` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for risk_approveResult
+-- ----------------------------
+DROP TABLE IF EXISTS `risk_approveResult`;
+CREATE TABLE `risk_approveResult`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户唯一编号',
+  `bizNo` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '业务流水号，此字段由商户生成，需确保唯一性，用于定位每一次请求',
+  `applyId` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '进件编号',
+  `payIndex` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '借款次数',
+  `approveResult` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '风控结果',
+  `approveCredit` int(10) NULL DEFAULT NULL COMMENT '授信额度，单位：元',
+  `approveAPR` varchar(4) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '授信年化利率（%）',
+  `approveDPR` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '授信日利率（‱）',
+  `minLoanTerm` int(2) NULL DEFAULT NULL COMMENT '最小贷款周期',
+  `maxLoanTerm` int(2) NULL DEFAULT NULL COMMENT '最大贷款周期',
+  `refuseCode` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '拒绝原因编码',
+  `refuseReason` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '拒绝原因明细说明',
+  `approveTime` datetime(0) NULL DEFAULT NULL COMMENT '审批时间',
+  `createTime` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `remark` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for risk_certification
+-- ----------------------------
+DROP TABLE IF EXISTS `risk_certification`;
+CREATE TABLE `risk_certification`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户ID',
+  `idCard` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证号码',
+  `mobile` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `certificationType` varchar(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '认证类型',
+  `certificationItem` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '认证项（param）',
+  `certificationResult` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '认证结果',
+  `certificationLimit` datetime(0) NOT NULL COMMENT '认证有效期',
+  `flag` int(2) NULL DEFAULT NULL COMMENT '标识',
+  `remark` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `creatTime` datetime(0) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for risk_certificationUserInfo
+-- ----------------------------
+DROP TABLE IF EXISTS `risk_certificationUserInfo`;
+CREATE TABLE `risk_certificationUserInfo`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `userId` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户ID',
+  `mobile` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册手机号',
+  `realName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `nation` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '民族（OCR）',
+  `age` int(3) NULL DEFAULT NULL COMMENT '年龄',
+  `addressPermanent` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '户籍地址（OCR）',
+  `idCardValidDate` datetime(0) NULL DEFAULT NULL COMMENT '身份证有效期（OCR）',
+  `maritalStatus` int(1) NULL DEFAULT NULL COMMENT '婚姻状态 (1已婚 2未婚 3离异 4丧偶)',
+  `education` int(1) NULL DEFAULT NULL COMMENT '文化水平(1硕士及以上2本科3大专4中专5高中6初中及以下)',
+  `institution` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学校院系名称',
+  `addressWork` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工作地址',
+  `addressHome` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '家庭地址',
+  `payday` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '发薪日',
+  `companyName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '单位名称',
+  `companyPhone` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '单位电话',
+  `sosContactName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '紧急联系人姓名',
+  `sosContactRelation` int(1) NULL DEFAULT NULL COMMENT '紧急联系人 关系 (1配偶 2父母 3子女)',
+  `sosContactPhone` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '紧急联系人联系方式',
+  `sosContactName1` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '紧急联系人1姓名',
+  `sosContactRelation1` int(1) NULL DEFAULT NULL COMMENT '紧急联系人1 关系(4兄弟姐妹5同事6同学7朋友8亲戚)',
+  `sosContactPhone1` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '紧急联系人1联系方式',
+  `lastLoginIp` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后登录 ip',
+  `longitude` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后更新gps 经度',
+  `latitude` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后更新gps 纬度',
+  `gpsAddress` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后更新gps 地址',
+  `regOs` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册设备[系统,版本,手机型号]',
+  `regAppVersion` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册app 版本',
+  `regFrom` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '进件引流渠道(建议0为自然流量，其他渠道请提供对应码表)',
+  `regIp` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '注册ip',
+  `bankCard` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '银行卡号',
+  `bankMobile` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '银行卡预留手机号',
+  `createTime` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
