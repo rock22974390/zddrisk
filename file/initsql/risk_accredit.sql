b0add87f8c36ea9716153b6af267b59d995b554d
rock <22974390@qq.com>
2018-11-09 10:35:57 +0800

    add sql init file

---
+++ file/initsql/risk_accredit.sql
@@ -0,0 +1,4 @@
+INSERT INTO `risk_accredit`(`id`, `orderId`, `userId`, `accreditInfo`, `createTime`) VALUES (1, '450703199410121852', '???', NULL, NULL);
+INSERT INTO `risk_accredit`(`id`, `orderId`, `userId`, `accreditInfo`, `createTime`) VALUES (2, '450703199410121852', '???', NULL, NULL);
+INSERT INTO `risk_accredit`(`id`, `orderId`, `userId`, `accreditInfo`, `createTime`) VALUES (3, '450703199410121852', '13410701192', NULL, NULL);
+INSERT INTO `risk_accredit`(`id`, `orderId`, `userId`, `accreditInfo`, `createTime`) VALUES (4, '450703199410121852', '黄潇振', NULL, NULL);
